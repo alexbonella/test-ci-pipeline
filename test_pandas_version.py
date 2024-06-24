54caf7c5336517daba2612637ff69816172c3968
Alexander <alexbonella806@gmail.com>
2024-06-24 07:35:35 -0500

    delete row

--- test_pandas_version.py
+++ test_pandas_version.py
@@ -12,7 +12,6 @@ def test_pandas_version():
     ''' Use an assertion to check the output of pd.__version__ '''
     assert pd.__version__ in [PANDAS_VERSION]
 
-
 if __name__ == "__main__":
     test_pandas_version()
     print("Pandas version is correct!")
