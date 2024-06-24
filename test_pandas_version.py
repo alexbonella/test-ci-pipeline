de7ec342a185e8e24df0cbe004f48324f2f978fe
Alexander <alexbonella806@gmail.com>
2024-06-24 07:44:17 -0500

    Add new blanck lines

--- test_pandas_version.py
+++ test_pandas_version.py
@@ -12,6 +12,7 @@ def test_pandas_version():
     ''' Use an assertion to check the output of pd.__version__ '''
     assert pd.__version__ == [PANDAS_VERSION]
 
+
 if __name__ == "__main__":
     test_pandas_version()
     print("Pandas version is correct!")
