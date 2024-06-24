ceaebdbeac76a7a98df55f23ee4a709dccc0cd1f
Alexander <alexbonella806@gmail.com>
2024-06-24 07:42:30 -0500

    Fix Operator

--- test_pandas_version.py
+++ test_pandas_version.py
@@ -10,7 +10,7 @@ PANDAS_VERSION = "1.5.3"
 
 def test_pandas_version():
     ''' Use an assertion to check the output of pd.__version__ '''
-    assert pd.__version__ in [PANDAS_VERSION]
+    assert pd.__version__ == [PANDAS_VERSION]
 
 if __name__ == "__main__":
     test_pandas_version()
